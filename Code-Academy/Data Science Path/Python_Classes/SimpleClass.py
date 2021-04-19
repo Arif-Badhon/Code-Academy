416dfbb6033b866859ac1c63e76a4b4ad594d894
Arif Uz Zaman Badhon <md.arifuz.zaman@g.bracu.ac.bd>
2021-04-20 03:54:52 +0600

    Create SimpleClass.py

---
+++ Code-Academy/Data Science Path/Python_Classes/SimpleClass.py
@@ -0,0 +1,13 @@
+#This programme calculate area for given objects
+
+class Circle:
+  pi = 3.14
+  def area(self, radius):
+    area = Circle.pi * radius ** 2
+    return area
+
+circle = Circle()
+
+pizza_area = circle.area(6)
+teaching_table_area = circle.area(18)
+round_room_area = circle.area(5730)
