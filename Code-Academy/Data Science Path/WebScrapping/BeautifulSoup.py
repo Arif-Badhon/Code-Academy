6295dcb6e2c9912ed503509504a9758fbd762f79
Arif Uz Zaman Badhon <md.arifuz.zaman@g.bracu.ac.bd>
2021-04-27 01:14:09 +0600

    Create BeautifulSoup.py

---
+++ Code-Academy/Data Science Path/WebScrapping/BeautifulSoup.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+
+webpage_response = requests.get('https://content.codecademy.com/courses/beautifulsoup/shellter.html')
+
+webpage = webpage_response.content
+soup = BeautifulSoup(webpage)
