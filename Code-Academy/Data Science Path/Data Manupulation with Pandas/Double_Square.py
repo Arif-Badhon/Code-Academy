1d2323c0aab13526dfdbe0aaf9b682643bbfd665
Arif Uz Zaman Badhon <md.arifuz.zaman@g.bracu.ac.bd>
2021-04-28 23:45:09 +0600

    Create Double_Square.py

---
+++ Code-Academy/Data Science Path/Data Manupulation with Pandas/Double_Square.py
@@ -0,0 +1,7 @@
+#Create a lambda function named double_square that takes an input named num. The function should return twice the square of num
+
+#Write your lambda function here
+double_square = lambda num: 2*(num**2)
+
+print double_square(5)
+print double_square(3)
