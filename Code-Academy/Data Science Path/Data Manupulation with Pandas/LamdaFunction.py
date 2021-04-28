6e696a56a2420c0beaa8c1e970cc66b7217d0a79
Arif Uz Zaman Badhon <md.arifuz.zaman@g.bracu.ac.bd>
2021-04-28 21:01:03 +0600

    Create LamdaFunction.py

---
+++ Code-Academy/Data Science Path/Data Manupulation with Pandas/LamdaFunction.py
@@ -0,0 +1,16 @@
+add_two = lambda my_input: my_input + 2
+
+print(add_two(3))   ##ans:5
+print(add_two(100)) ##ans:102
+print(add_two(-2))  ##ans:0
+
+
+is_substring = lambda my_string: my_string in "This is the master string"
+
+print(is_substring('I'))  ##False
+print(is_substring('am')) ##False
+print(is_substring('the')) ##True
+print(is_substring('master')) ##True
+
+
+
