538fa5a38dc7770283d92b728169d1be14ddf959
Arif Uz Zaman Badhon <md.arifuz.zaman@g.bracu.ac.bd>
2021-04-28 23:38:09 +0600

    Create Ones_place.py

---
+++ Code-Academy/Data Science Path/Data Manupulation with Pandas/Ones_place.py
@@ -0,0 +1,7 @@
+#Create a lambda function named ones_place that returns the ones’ place of the input num
+
+#Write your lambda function here
+ones_place = lambda num: num%10
+
+print ones_place(123)
+print ones_place(4)
