86dd321664c4ad16725abb6f15912a081930e375
Arif Uz Zaman Badhon <md.arifuz.zaman@g.bracu.ac.bd>
2021-05-04 03:48:04 +0600

    Create Introduction.py

---
+++ Code-Academy/Data Science Path/Data Manupulation with Pandas/Multiple Dataframe/Introduction.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+orders = pd.read_csv('orders.csv')
+print(orders)
+products = pd.read_csv('products.csv')
+print(products)
+customers = pd.read_csv('customers.csv')
+print(customers)
