cf3564d776ee3087e94ec0aec0074314f3daf884
Arif Uz Zaman Badhon <md.arifuz.zaman@g.bracu.ac.bd>
2021-05-03 23:38:01 +0600

    Create CalculatingColumnStatistics.py

---
+++ Code-Academy/Data Science Path/Data Manupulation with Pandas/Aggregates in Pandas/CalculatingColumnStatistics.py
@@ -0,0 +1,9 @@
+import codecademylib
+import pandas as pd
+
+orders = pd.read_csv('orders.csv')
+#Our finance department wants to know the price of the most expensive pair of shoes purchased. Save your answer to the variable most_expensive
+most_expensive = orders.price.max()
+#Our fashion department wants to know how many different colors of shoes we are selling. Save your answer to the variable num_colors
+num_colors = orders.shoe_color.nunique()
+print(orders.head(10))
