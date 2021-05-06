5546bb333eb235a58cb18dc498171d204217e988
Arif Uz Zaman Badhon <md.arifuz.zaman@g.bracu.ac.bd>
2021-05-07 00:09:41 +0600

    Update MultipleTablesInPandas.py

--- Code-Academy/Data Science Path/Data Manupulation with Pandas/Multiple Dataframe/MultipleTablesInPandas.py
+++ Code-Academy/Data Science Path/Data Manupulation with Pandas/Multiple Dataframe/MultipleTablesInPandas.py
@@ -1,1 +1,15 @@
+import codecademylib
+import pandas as pd
 
+orders = pd.read_csv('orders.csv')
+products = pd.read_csv('products.csv')
+
+#In script.py, you’ll find two DataFrames: products and orders. Inspect these DataFrames using print.
+print(orders)
+print(products)
+
+#Merge orders and products and save it to the variable merged_df.
+
+merged_df = pd.merge(orders, products)
+
+print(merged_df)
