539e541ea2dba47c8656b20c80fb0dc8a680917a
Arif Uz Zaman Badhon <md.arifuz.zaman@g.bracu.ac.bd>
2021-05-06 23:54:04 +0600

    Update MergeonSpecificColumns.py

--- Code-Academy/Data Science Path/Data Manupulation with Pandas/Multiple Dataframe/MergeonSpecificColumns.py
+++ Code-Academy/Data Science Path/Data Manupulation with Pandas/Multiple Dataframe/MergeonSpecificColumns.py
@@ -12,3 +12,21 @@ orders_products = pd.merge(
   products.rename(columns={'id': 'product_id'})
 )
 print(orders_products)
+
+
+import codecademylib
+import pandas as pd
+
+orders = pd.read_csv('orders.csv')
+products = pd.read_csv('products.csv')
+
+# Use left_on and right_on to merge orders and products
+orders_products = pd.merge(
+	orders,
+	products,
+	left_on = 'product_id',
+	right_on = 'id',
+	suffixes = ['_orders', '_products']
+)
+
+print(orders_products)
