ee8a61571b050789a48754e334b1d4990ddf3952
Arif Uz Zaman Badhon <md.arifuz.zaman@g.bracu.ac.bd>
2021-05-31 15:15:05 +0600

    Update DealingWithDuplicates.py

--- Code-Academy/Data Science Path/CleanDataWithPython/DealingWithDuplicates.py
+++ Code-Academy/Data Science Path/CleanDataWithPython/DealingWithDuplicates.py
@@ -1,1 +1,12 @@
+import codecademylib3_seaborn
+import pandas as pd
+from students import students
 
+print(students)
+
+duplicates = students.duplicated()
+duplicates.value_counts()
+
+students = students.drop_duplicates()
+
+duplicates = students.duplicated()
